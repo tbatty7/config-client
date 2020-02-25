02ed76281a60caf81130ff72a135f663b86f4a58
Tim B <tbatty7@gmail.com>
2020-02-24 21:02:30 -0500

    initial commit of simple config server client

---
+++ src/main/java/com/battybuilds/configClient/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.battybuilds.configClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
